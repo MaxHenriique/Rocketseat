cc815d63c952b7214a451686d2ef21779b442df5
Max Henrique <contato.maxhenrique@gmail.com>
2022-10-08 14:31:00 -0300

    Header responsivo

--- dashgo/src/components/Header/Profile.tsx
+++ dashgo/src/components/Header/Profile.tsx
@@ -1,16 +1,25 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+export function Profile({showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
           <Text>Max Henrique</Text>
           <Text color="gray.300" fontSize="small">
-              contato.maxhenrique@gmail.com
+            contato.maxhenrique@gmail.com
           </Text>
-      </Box>
+        </Box>
+      )}
       
-      <Avatar size="md" name="Max Henrique" src="https://avatars.githubusercontent.com/u/101279529?v=4" />
+      <Avatar 
+        size="md" 
+        name="Max Henrique" 
+        src="https://avatars.githubusercontent.com/u/101279529?v=4" 
+      />
     </Flex>
   );
 }
